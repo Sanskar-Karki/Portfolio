128bc5b36c62f8f2b21dfdbf6fe43d9a773d885a
Sans <sanskarkarki20@gmail.com>
2025-03-09 17:23:28 +0545

    t

--- src/components/home/Data.jsx
+++ src/components/home/Data.jsx
@@ -3,7 +3,7 @@ import Cv from '../../assets/Sanskar_karki_CV.pdf'
 const Data = () => {
   return (
     <div className='home__data'>
-      <div className="home__title">Hi, I'm Sanskar Karki 👋
+      <div className="home__title">Hi, I'm Sanskar Karki 👋👋👋
       </div>
       <h3 className="home__subtitle">Web Developer</h3>
       <p className='home__description'>I specialize in frontend development, building responsive and user-friendly applications.</p>
